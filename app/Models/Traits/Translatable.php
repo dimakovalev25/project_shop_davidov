7f1a33a12580af8335256487bcb7f1f518679f31
dimakovalev <d.nozdryn@gmail.com>
2023-07-26 13:43:42 +0300

    used Seeder

--- app/Models/Traits/Translatable.php
+++ app/Models/Traits/Translatable.php
@@ -9,16 +9,23 @@ trait Translatable
 
     protected $defaultLocale = 'en';
 
-    public function __($fieldName)
+    public function __($originFieldName)
     {
         $locale = App::getLocale() ?? $this->defaultLocale;
+
         if ($locale === 'ru') {
-            $fieldName .= '_ru';
+            $fieldName = $originFieldName.'_ru';
+        } else {
+            $fieldName = $originFieldName;
         }
 
 /*        if (in_array($fieldName, $this->attributes)) {
             throw new \LogicException('no such attribute');
         } */
+
+        if ($locale === 'ru' && (is_null($this->$fieldName) || empty($this->$fieldName))) {
+            return  $this->$originFieldName;
+        }
 
         return $this->$fieldName;
 
---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CurrencySeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('currencies')->truncate();
+        DB::table('currencies')->insert([
+            [
+                'code'=>'USD',
+                'symbol' => '$',
+                'is_main' => 0,
+                'rate' => 0.4
+
+            ],
+            [
+                'code'=>'EUR',
+                'symbol' => '€  ',
+                'is_main' => 0,
+                'rate' => 0.36
+
+            ],
+            [
+                'code'=>'BY',
+                'symbol' => 'Br',
+                'is_main' => 1,
+                'rate' => 1
+
+            ],
+        ]);
+    }
+}
